f7a5f88fbd99f09aa748bd8b8ae96d7b793170a2
Jahmaine Shiloh <55574810+Skullomanic84@users.noreply.github.com>
2025-11-13 18:42:59 +0200

    Added unsecure and secure routes in auth.route.js file. Added more validation in the validator index.js file.

--- src/validator/index.js
+++ src/validator/index.js
@@ -48,4 +48,30 @@ const userLoginValidator = () => {
   ];
 };
 
-export { userRegisterValidator, userLoginValidator };
+const userChangeCurrentPasswordValidator = () => {
+  return [
+    body('oldPassword').notEmpty().withMessage('Old password is required'),
+    body('newPassword').notEmpty().withMessage('New password is required'),
+  ];
+};
+
+const userForgotPasswordValidator = () => {
+  return [
+    body('email').notEmpty().withMessage('Email is required') .isEmail().withMessage('Email is invalid'),
+  ];
+};
+
+const userResetForgotPasswordValidator = () => {
+  return [
+    body('newPassword').notEmpty().withMessage('Password is required'),
+    body('confirmPassword').notEmpty().withMessage('Confirm password is required'),
+  ];
+};
+
+
+export { 
+  userRegisterValidator, 
+  userLoginValidator, 
+  userChangeCurrentPasswordValidator, 
+  userForgotPasswordValidator 
+};
